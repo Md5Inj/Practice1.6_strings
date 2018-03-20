c3b202791d4c298c82f150d8701f8c6b687b548d
Maxim Romashko <md5inj@localhost>
2018-03-20 16:42:33 +0300

    Добавьте файлы проекта.

---
+++ Practice1.6_strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1._6_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
